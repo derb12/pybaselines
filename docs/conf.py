64da18203521bc807b22fa8483433700359f206b
derb12 <55961724+derb12@users.noreply.github.com>
2021-08-02 17:56:13 -0400

    MAINT: Bump version v0.4.1 -> v0.5.0
    [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ author = "Donald Erb"
 # the built documents.
 #
 # The short X.Y version.
-version = '0.4.1'
+version = '0.5.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
