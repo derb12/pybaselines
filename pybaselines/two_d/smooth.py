5765527953de2d933c7664633ad61ee8f6d1e633
Donnie Erb <55961724+derb12@users.noreply.github.com>
2024-01-16 20:47:36 -0500

    FEAT: Add 2D version of noise_median
    Still need to work out minor details for it.

--- pybaselines/two_d/smooth.py
+++ pybaselines/two_d/smooth.py
@@ -6,5 +6,68 @@ Created on April 8, 2023
 
 """
 
-class _Smooth:
-    pass
+from scipy.ndimage import gaussian_filter, median_filter
+
+from ._algorithm_setup import _Algorithm2D, _optimize_window
+
+class _Smooth(_Algorithm2D):
+    """A base class for all smoothing algorithms."""
+
+    @_Algorithm2D._register
+    def noise_median(self, data, half_window=None, smooth_half_window=None, sigma=None,
+                     **pad_kwargs):
+        """
+        The noise-median method for baseline identification.
+
+        Assumes the baseline can be considered as the median value within a moving
+        window, and the resulting baseline is then smoothed with a Gaussian kernel.
+
+        Parameters
+        ----------
+        data : array-like, shape (N,)
+            The y-values of the measured data, with N data points.
+        half_window : int, optional
+            The index-based size to use for the median window. The total window
+            size will range from [-half_window, ..., half_window] with size
+            2 * half_window + 1. Default is None, which will use twice the output from
+            :func:`.optimize_window`, which is an okay starting value.
+        smooth_half_window : int, optional
+            The half window to use for smoothing. Default is None, which will use
+            the same value as `half_window`.
+        sigma : float, optional
+            The standard deviation of the smoothing Gaussian kernel. Default is None,
+            which will use (2 * `smooth_half_window` + 1) / 6.
+        **pad_kwargs
+            Additional keyword arguments to pass to :func:`.pad_edges` for padding
+            the edges of the data to prevent edge effects from convolution.
+
+        Returns
+        -------
+        baseline : numpy.ndarray, shape (N,)
+            The calculated and smoothed baseline.
+        dict
+            An empty dictionary, just to match the output of all other algorithms.
+
+        References
+        ----------
+        Friedrichs, M., A model-free algorithm for the removal of baseline
+        artifacts. J. Biomolecular NMR, 1995, 5, 147-153.
+
+        """
+        if half_window is None:
+            half_window = 2 * _optimize_window(data)
+        window_size = 2 * half_window + 1
+        median = median_filter(
+            self._setup_smooth(data, half_window, **pad_kwargs),
+            [window_size, window_size], mode='nearest'
+        )
+        if smooth_half_window is None:
+            smooth_window = window_size
+        else:
+            smooth_window = 2 * smooth_half_window + 1
+        if sigma is None:
+            # the gaussian kernel will includes +- 3 sigma
+            sigma = smooth_window / 6
+
+        baseline = gaussian_filter(median, sigma, truncate=smooth_window)  # TODO check truncate value
+        return baseline[half_window:-half_window, half_window:-half_window], {}
