FIX: Corrected airpls weighting
The weighting equation for airpls was misprinted in its journal article, so changed to the correct weighting scheme. Also improved overflow avoidance for airpls so that try except blocks are no longer needed, and slightly sped up asls weighting.

pybaselines/_weighting.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 """Contains various weighting schemes used in pybaselines."""
 
+import warnings
+
 import numpy as np
 from scipy.special import expit
 
-from .utils import _MIN_FLOAT
+from .utils import _MIN_FLOAT, ParameterWarning
 
 
 def _asls(y, baseline, p):
@@ -22,7 +24,7 @@ def _asls(y, baseline, p):
     p : float
         The penalizing weighting factor. Must be between 0 and 1. Values greater
         than the baseline will be given `p` weight, and values less than the baseline
-        will be given `p - 1` weight.
+        will be given `1 - p` weight.
 
     Returns
     -------
@@ -38,9 +40,81 @@ def _asls(y, baseline, p):
     asymmetric least squares method, Analytical Methods, 2014, 6(12), 4402-4407.
 
     """
-    mask = y > baseline
-    weights = p * mask + (1 - p) * (~mask)
+    weights = np.where(y > baseline, p, 1 - p)
     return weights
+
+
+def _airpls(y, baseline, iteration):
+    """
+    The weighting for adaptive iteratively reweighted penalized least squares (airPLS).
+
+    Parameters
+    ----------
+    y : numpy.ndarray, shape (N,)
+        The measured data.
+    baseline : numpy.ndarray, shape (N,)
+        The calculated baseline.
+    iteration : int
+        The iteration number. Should be 1-based, such that the first iteration is 1
+        instead of 0.
+
+    Returns
+    -------
+    weights : numpy.ndarray, shape (N,)
+        The calculated weights.
+    residual_l1_norm : float
+        The L1 norm of the negative residuals, used to calculate the exit criteria
+        for the airPLS algorithm.
+    exit_early : bool
+        Designates if there is a potential error with the calculation such that no further
+        iterations should be performed.
+
+    References
+    ----------
+    Zhang, Z.M., et al. Baseline correction using adaptive iteratively
+    reweighted penalized least squares. Analyst, 2010, 135(5), 1138-1146.
+
+    Notes
+    -----
+    Equation 9 in the original algorithm was misprinted according to the author
+    (https://github.com/zmzhang/airPLS/issues/8), so the correct weighting is used here.
+
+    The pybaselines weighting differs from the original airPLS algorithm by normalizing the
+    weights by their maximum value to ensure that weights are within [0, 1]; the original
+    algorithm is defined such that all weights are greater than or equal to 1 for negative
+    residuals, which would differ from other Whittaker-smoothing based algorithms and could
+    potentially cause issues if combining with optimizer functions.
+
+    """
+    residual = y - baseline
+    neg_mask = residual < 0
+    neg_residual = residual[neg_mask]
+    if neg_residual.size < 2:
+        exit_early = True
+        warnings.warn(
+            ('almost all baseline points are below the data, indicating that "tol"'
+             ' is too low and/or "max_iter" is too high'), ParameterWarning,
+             stacklevel=2
+        )
+        return np.zeros_like(y), 0.0, exit_early
+    else:
+        exit_early = False
+
+    residual_l1_norm = abs(neg_residual.sum())
+    # clip from [0, log(max dtype)] since the positive residuals (negative values) do not matter
+    log_max = np.log(np.finfo(y.dtype).max)
+    inner = np.clip(
+        (-iteration / residual_l1_norm) * neg_residual,
+        a_min=0,
+        a_max=log_max - np.spacing(log_max)
+    )
+    new_weights = np.exp(inner)
+    # Not stated in the paper, but without dividing by the maximum weight, the
+    # calculated weights are all greater than or equal to 1
+    weights = np.zeros_like(y)
+    weights[neg_mask] = new_weights / new_weights.max()
+
+    return weights, residual_l1_norm, exit_early
 
 
 def _safe_std(array, **kwargs):
@@ -228,7 +302,7 @@ def _psalsa(y, baseline, p, k, shape_y):
     p : float
         The penalizing weighting factor. Must be between 0 and 1. Values greater
         than the baseline will be given `p` weight, and values less than the baseline
-        will be given `p - 1` weight.
+        will be given `1 - p` weight.
     k : float
         A factor that controls the exponential decay of the weights for baseline
         values greater than the data. Should be approximately the height at which
@@ -270,7 +344,7 @@ def _derpsalsa(y, baseline, p, k, shape_y, partial_weights):
     p : float
         The penalizing weighting factor. Must be between 0 and 1. Values greater
         than the baseline will be given `p` weight, and values less than the baseline
-        will be given `p - 1` weight.
+        will be given `1 - p` weight.
     k : float
         A factor that controls the exponential decay of the weights for baseline
         values greater than the data. Should be approximately the height at which

pybaselines/two_d/spline.py
@@ -39,7 +39,7 @@ class _Spline(_Algorithm2D):
         p : float, optional
             The penalizing weighting factor. Must be between 0 and 1. Values greater
             than the baseline will be given `p` weight, and values less than the baseline
-            will be given `p - 1` weight. Used to set the initial weights before performing
+            will be given `1 - p` weight. Used to set the initial weights before performing
             expectation-maximization. Default is 1e-2.
         num_knots : int or Sequence[int, int], optional
             The number of knots for the splines along the rows and columns, respectively. If a
@@ -292,7 +292,7 @@ class _Spline(_Algorithm2D):
         p : float, optional
             The penalizing weighting factor. Must be between 0 and 1. Values greater
             than the baseline will be given `p` weight, and values less than the baseline
-            will be given `p - 1` weight. Default is 1e-2.
+            will be given `1 - p` weight. Default is 1e-2.
         num_knots : int or Sequence[int, int], optional
             The number of knots for the splines along the rows and columns, respectively. If a
             single value is given, both will use the same value. Default is 25.
@@ -383,7 +383,7 @@ class _Spline(_Algorithm2D):
         p : float, optional
             The penalizing weighting factor. Must be between 0 and 1. Values greater
             than the baseline will be given `p` weight, and values less than the baseline
-            will be given `p - 1` weight. Default is 1e-2.
+            will be given `1 - p` weight. Default is 1e-2.
         lam_1 : float, optional
             The smoothing parameter for the first derivative of the residual. Default is 1e-4.
         num_knots : int, optional
@@ -529,48 +529,22 @@ class _Spline(_Algorithm2D):
 
         """
         y, weight_array = self._setup_spline(
-            data, weights, spline_degree, num_knots, True, diff_order, lam, copy_weights=True
+            data, weights, spline_degree, num_knots, True, diff_order, lam
         )
 
         y_l1_norm = np.abs(y).sum()
         tol_history = np.empty(max_iter + 1)
         for i in range(1, max_iter + 2):
-            try:
-                output = self.pspline.solve(y, weight_array)
-            except np.linalg.LinAlgError:
-                warnings.warn(
-                    ('error occurred during fitting, indicating that "tol"'
-                     ' is too low, "max_iter" is too high, or "lam" is too high'),
-                    ParameterWarning, stacklevel=2
-                )
+            baseline = self.pspline.solve(y, weight_array)
+            new_weights, residual_l1_norm, exit_early = _weighting._airpls(y, baseline, i)
+            if exit_early:
                 i -= 1  # reduce i so that output tol_history indexing is correct
                 break
-            else:
-                baseline = output
-
-            residual = y - baseline
-            neg_mask = residual < 0
-            neg_residual = residual[neg_mask]
-            if len(neg_residual) < 2:
-                # exit if there are < 2 negative residuals since all points or all but one
-                # point would get a weight of 0, which fails the solver
-                warnings.warn(
-                    ('almost all baseline points are below the data, indicating that "tol"'
-                     ' is too low and/or "max_iter" is too high'), ParameterWarning,
-                    stacklevel=2
-                )
-                i -= 1  # reduce i so that output tol_history indexing is correct
-                break
-
-            residual_l1_norm = abs(neg_residual.sum())
             calc_difference = residual_l1_norm / y_l1_norm
             tol_history[i - 1] = calc_difference
             if calc_difference < tol:
                 break
-            # only use negative residual in exp to avoid exponential overflow warnings
-            # and accidently creating a weight of nan (inf * 0 = nan)
-            weight_array[neg_mask] = np.exp(i * neg_residual / residual_l1_norm)
-            weight_array[~neg_mask] = 0
+            weight_array = new_weights
 
         params = {'weights': weight_array, 'tol_history': tol_history[:i]}
 
@@ -758,7 +732,7 @@ class _Spline(_Algorithm2D):
         p : float, optional
             The penalizing weighting factor. Must be between 0 and 1. Values greater
             than the baseline will be given `p` weight, and values less than the baseline
-            will be given `p - 1` weight. Default is 0.5.
+            will be given `1 - p` weight. Default is 0.5.
         k : float, optional
             A factor that controls the exponential decay of the weights for baseline
             values greater than the data. Should be approximately the height at which

pybaselines/two_d/whittaker.py
@@ -38,7 +38,7 @@ class _Whittaker(_Algorithm2D):
         p : float, optional
             The penalizing weighting factor. Must be between 0 and 1. Values greater
             than the baseline will be given `p` weight, and values less than the baseline
-            will be given `p - 1` weight. Default is 1e-2.
+            will be given `1 - p` weight. Default is 1e-2.
         diff_order : int or Sequence[int, int], optional
             The order of the differential matrix for the rows and columns, respectively. If
             a single value is given, both will use the same value. Must be greater than 0.
@@ -143,7 +143,7 @@ class _Whittaker(_Algorithm2D):
         p : float, optional
             The penalizing weighting factor. Must be between 0 and 1. Values greater
             than the baseline will be given `p` weight, and values less than the baseline
-            will be given `p - 1` weight. Default is 1e-2.
+            will be given `1 - p` weight. Default is 1e-2.
         lam_1 : float or Sequence[float, float], optional
             The smoothing parameter for the rows and columns, respectively, of the first
             derivative of the residual. Default is 1e-4.
@@ -282,50 +282,22 @@ class _Whittaker(_Algorithm2D):
 
         """
         y, weight_array = self._setup_whittaker(
-            data, lam, diff_order, weights, copy_weights=True, num_eigens=num_eigens
+            data, lam, diff_order, weights, num_eigens=num_eigens
 
         )
         y_l1_norm = np.abs(y).sum()
         tol_history = np.empty(max_iter + 1)
-        # Have to have extensive error handling since the weights can all become
-        # very small due to the exp(i) term if too many iterations are performed;
-        # checking the negative residual length usually prevents any errors, but
-        # sometimes not so have to also catch any errors from the solvers
         for i in range(1, max_iter + 2):
-            try:
-                output = self.whittaker_system.solve(y, weight_array)
-            except np.linalg.LinAlgError:
-                warnings.warn(
-                    ('error occurred during fitting, indicating that "tol"'
-                     ' is too low, "max_iter" is too high, or "lam" is too high'),
-                    ParameterWarning, stacklevel=2
-                )
+            baseline = self.whittaker_system.solve(y, weight_array)
+            new_weights, residual_l1_norm, exit_early = _weighting._airpls(y, baseline, i)
+            if exit_early:
                 i -= 1  # reduce i so that output tol_history indexing is correct
                 break
-            else:
-                baseline = output
-            residual = y - baseline
-            neg_mask = residual < 0
-            neg_residual = residual[neg_mask]
-            if neg_residual.size < 2:
-                # exit if there are < 2 negative residuals since all points or all but one
-                # point would get a weight of 0, which fails the solver
-                warnings.warn(
-                    ('almost all baseline points are below the data, indicating that "tol"'
-                     ' is too low and/or "max_iter" is too high'), ParameterWarning, stacklevel=2
-                )
-                i -= 1  # reduce i so that output tol_history indexing is correct
-                break
-
-            residual_l1_norm = abs(neg_residual.sum())
             calc_difference = residual_l1_norm / y_l1_norm
             tol_history[i - 1] = calc_difference
             if calc_difference < tol:
                 break
-            # only use negative residual in exp to avoid exponential overflow warnings
-            # and accidently creating a weight of nan (inf * 0 = nan)
-            weight_array[neg_mask] = np.exp(i * neg_residual / residual_l1_norm)
-            weight_array[~neg_mask] = 0
+            weight_array = new_weights
 
         params = {'tol_history': tol_history[:i]}
         if self.whittaker_system._using_svd:
@@ -745,7 +717,7 @@ class _Whittaker(_Algorithm2D):
         p : float, optional
             The penalizing weighting factor. Must be between 0 and 1. Values greater
             than the baseline will be given `p` weight, and values less than the baseline
-            will be given `p - 1` weight. Default is 0.5.
+            will be given `1 - p` weight. Default is 0.5.
         k : float, optional
             A factor that controls the exponential decay of the weights for baseline
             values greater than the data. Should be approximately the height at which

pybaselines/whittaker.py
@@ -38,7 +38,7 @@ class _Whittaker(_Algorithm):
         p : float, optional
             The penalizing weighting factor. Must be between 0 and 1. Values greater
             than the baseline will be given `p` weight, and values less than the baseline
-            will be given `p - 1` weight. Default is 1e-2.
+            will be given `1 - p` weight. Default is 1e-2.
         diff_order : int, optional
             The order of the differential matrix. Must be greater than 0. Default is 2
             (second order differential matrix). Typical values are 2 or 1.
@@ -117,7 +117,7 @@ class _Whittaker(_Algorithm):
         p : float, optional
             The penalizing weighting factor. Must be between 0 and 1. Values greater
             than the baseline will be given `p` weight, and values less than the baseline
-            will be given `p - 1` weight. Default is 1e-2.
+            will be given `1 - p` weight. Default is 1e-2.
         lam_1 : float, optional
             The smoothing parameter for the first derivative of the residual. Default is 1e-4.
         max_iter : int, optional
@@ -245,54 +245,23 @@ class _Whittaker(_Algorithm):
         reweighted penalized least squares. Analyst, 2010, 135(5), 1138-1146.
 
         """
-        y, weight_array = self._setup_whittaker(
-            data, lam, diff_order, weights, copy_weights=True
-        )
+        y, weight_array = self._setup_whittaker(data, lam, diff_order, weights)
         y_l1_norm = np.abs(y).sum()
         tol_history = np.empty(max_iter + 1)
-        # Have to have extensive error handling since the weights can all become
-        # very small due to the exp(i) term if too many iterations are performed;
-        # checking the negative residual length usually prevents any errors, but
-        # sometimes not so have to also catch any errors from the solvers
         for i in range(1, max_iter + 2):
-            try:
-                output = self.whittaker_system.solve(
-                    self.whittaker_system.add_diagonal(weight_array), weight_array * y,
-                    overwrite_b=True, check_output=True
-                )
-            except np.linalg.LinAlgError:
-                warnings.warn(
-                    ('error occurred during fitting, indicating that "tol"'
-                     ' is too low, "max_iter" is too high, or "lam" is too high'),
-                    ParameterWarning, stacklevel=2
-                )
+            baseline = self.whittaker_system.solve(
+                self.whittaker_system.add_diagonal(weight_array), weight_array * y,
+                overwrite_b=True, check_output=True
+            )
+            new_weights, residual_l1_norm, exit_early = _weighting._airpls(y, baseline, i)
+            if exit_early:
                 i -= 1  # reduce i so that output tol_history indexing is correct
                 break
-            else:
-                baseline = output
-            residual = y - baseline
-            neg_mask = residual < 0
-            neg_residual = residual[neg_mask]
-            if len(neg_residual) < 2:
-                # exit if there are < 2 negative residuals since all points or all but one
-                # point would get a weight of 0, which fails the solver
-                warnings.warn(
-                    ('almost all baseline points are below the data, indicating that "tol"'
-                     ' is too low and/or "max_iter" is too high'), ParameterWarning,
-                     stacklevel=2
-                )
-                i -= 1  # reduce i so that output tol_history indexing is correct
-                break
-
-            residual_l1_norm = abs(neg_residual.sum())
             calc_difference = residual_l1_norm / y_l1_norm
             tol_history[i - 1] = calc_difference
             if calc_difference < tol:
                 break
-            # only use negative residual in exp to avoid exponential overflow warnings
-            # and accidently creating a weight of nan (inf * 0 = nan)
-            weight_array[neg_mask] = np.exp(i * neg_residual / residual_l1_norm)
-            weight_array[~neg_mask] = 0
+            weight_array = new_weights
 
         params = {'weights': weight_array, 'tol_history': tol_history[:i]}
 
@@ -660,7 +629,7 @@ class _Whittaker(_Algorithm):
         p : float, optional
             The penalizing weighting factor. Must be between 0 and 1. Values greater
             than the baseline will be given `p` weight, and values less than the baseline
-            will be given `p - 1` weight. Default is 0.5.
+            will be given `1 - p` weight. Default is 0.5.
         k : float, optional
             A factor that controls the exponential decay of the weights for baseline
             values greater than the data. Should be approximately the height at which
@@ -751,7 +720,7 @@ class _Whittaker(_Algorithm):
         p : float, optional
             The penalizing weighting factor. Must be between 0 and 1. Values greater
             than the baseline will be given `p` weight, and values less than the baseline
-            will be given `p - 1` weight. Default is 1e-2.
+            will be given `1 - p` weight. Default is 1e-2.
         k : float, optional
             A factor that controls the exponential decay of the weights for baseline
             values greater than the data. Should be approximately the height at which
@@ -868,7 +837,7 @@ def asls(data, lam=1e6, p=1e-2, diff_order=2, max_iter=50, tol=1e-3, weights=Non
     p : float, optional
         The penalizing weighting factor. Must be between 0 and 1. Values greater
         than the baseline will be given `p` weight, and values less than the baseline
-        will be given `p - 1` weight. Default is 1e-2.
+        will be given `1 - p` weight. Default is 1e-2.
     diff_order : int, optional
         The order of the differential matrix. Must be greater than 0. Default is 2
         (second order differential matrix). Typical values are 2 or 1.
@@ -935,7 +904,7 @@ def iasls(data, x_data=None, lam=1e6, p=1e-2, lam_1=1e-4, max_iter=50, tol=1e-3,
     p : float, optional
         The penalizing weighting factor. Must be between 0 and 1. Values greater
         than the baseline will be given `p` weight, and values less than the baseline
-        will be given `p - 1` weight. Default is 1e-2.
+        will be given `1 - p` weight. Default is 1e-2.
     lam_1 : float, optional
         The smoothing parameter for the first derivative of the residual. Default is 1e-4.
     max_iter : int, optional
@@ -1278,7 +1247,7 @@ def psalsa(data, lam=1e5, p=0.5, k=None, diff_order=2, max_iter=50, tol=1e-3,
     p : float, optional
         The penalizing weighting factor. Must be between 0 and 1. Values greater
         than the baseline will be given `p` weight, and values less than the baseline
-        will be given `p - 1` weight. Default is 0.5.
+        will be given `1 - p` weight. Default is 0.5.
     k : float, optional
         A factor that controls the exponential decay of the weights for baseline
         values greater than the data. Should be approximately the height at which
@@ -1352,7 +1321,7 @@ def derpsalsa(data, lam=1e6, p=0.01, k=None, diff_order=2, max_iter=50, tol=1e-3
     p : float, optional
         The penalizing weighting factor. Must be between 0 and 1. Values greater
         than the baseline will be given `p` weight, and values less than the baseline
-        will be given `p - 1` weight. Default is 1e-2.
+        will be given `1 - p` weight. Default is 1e-2.
     k : float, optional
         A factor that controls the exponential decay of the weights for baseline
         values greater than the data. Should be approximately the height at which

tests/test_weighting.py
@@ -5,7 +5,67 @@ import numpy as np
 from numpy.testing import assert_allclose
 import pytest
 
-from pybaselines import _weighting, utils
+from pybaselines import _weighting, utils, Baseline2D
+
+from .conftest import get_data, get_data2d
+
+
+def baseline_1d_normal():
+    """
+    Values for testing weights for a normally fit baseline.
+
+    Approximates the baseline as a first order polynomial fit, which is the
+    approximate fit for the first iteration of all Whittaker-smoothing-based
+    algorithms with a `diff_order` of 2 and a sufficiently high `lam` value.
+
+    """
+    x_data, y_data = get_data()
+    baseline = np.polynomial.Polynomial.fit(x_data, y_data, deg=1)(x_data)
+    return y_data, baseline
+
+
+def baseline_1d_all_above():
+    """Values for testing weights when all baseline points are above the data."""
+    x_data, y_data = get_data()
+    baseline = np.full_like(y_data, y_data.max() + 10)
+    return y_data, baseline
+
+
+def baseline_1d_all_below():
+    """Values for testing weights when all baseline points are below the data."""
+    x_data, y_data = get_data()
+    baseline = np.full_like(y_data, y_data.min() - 10)
+    return y_data, baseline
+
+
+def baseline_2d_normal():
+    """
+    Values for testing weights for a normally fit baseline.
+
+    Approximates the baseline as a first order polynomial fit, which is the
+    approximate fit for the first iteration of all Whittaker-smoothing-based
+    algorithms with a `diff_order` of 2 and a sufficiently high `lam` value.
+
+    """
+    x_data, z_data, y_data = get_data2d()
+    baseline = Baseline2D(x_data, z_data, check_finite=False, assume_sorted=True).poly(
+        y_data, poly_order=1
+    )[0]
+    return y_data, baseline
+
+
+def baseline_2d_all_above():
+    """Values for testing weights when all baseline points are above the data."""
+    x_data, z_data, y_data = get_data2d()
+    baseline = np.full_like(y_data, y_data.max() + 10)
+    return y_data, baseline
+
+
+def baseline_2d_all_below():
+    """Values for testing weights when all baseline points are below the data."""
+    x_data, z_data, y_data = get_data2d()
+    baseline = np.full_like(y_data, y_data.min() - 10)
+    return y_data, baseline
 
 
 def test_safe_std():
@@ -77,3 +137,207 @@ def test_quantile_weighting(quantile):
     expected_loss = numerator / denominator
 
     assert_allclose(calc_loss, expected_loss)
+
+
+@pytest.mark.parametrize('p', (0.01, 0.99))
+@pytest.mark.parametrize('two_d', (True, False))
+def test_asls_normal(p, two_d):
+    """Ensures asls weighting works as intented for a normal baseline."""
+    if two_d:
+        y_data, baseline = baseline_1d_normal()
+    else:
+        y_data, baseline = baseline_2d_normal()
+
+    weights = _weighting._asls(y_data, baseline, p)
+    expected_weights = np.where(y_data > baseline, p, 1 - p)
+
+    assert isinstance(weights, np.ndarray)
+    assert weights.shape == y_data.shape
+    assert_allclose(weights, expected_weights, rtol=1e-12, atol=1e-12)
+
+
+@pytest.mark.parametrize('p', (0.01, 0.99))
+@pytest.mark.parametrize('two_d', (True, False))
+def test_asls_all_above(p, two_d):
+    """Ensures asls weighting works as intented for a baseline with all points above the data."""
+    if two_d:
+        y_data, baseline = baseline_1d_all_above()
+    else:
+        y_data, baseline = baseline_2d_all_above()
+    weights = _weighting._asls(y_data, baseline, p)
+    expected_weights = np.full_like(y_data, 1 - p)
+
+    assert isinstance(weights, np.ndarray)
+    assert weights.shape == y_data.shape
+    assert_allclose(weights, expected_weights, rtol=1e-12, atol=1e-12)
+
+
+@pytest.mark.parametrize('p', (0.01, 0.99))
+@pytest.mark.parametrize('two_d', (True, False))
+def test_asls_all_below(p, two_d):
+    """Ensures asls weighting works as intented for a baseline with all points below the data."""
+    if two_d:
+        y_data, baseline = baseline_1d_all_below()
+    else:
+        y_data, baseline = baseline_2d_all_below()
+    weights = _weighting._asls(y_data, baseline, p)
+    expected_weights = np.full_like(y_data, p)
+
+    assert isinstance(weights, np.ndarray)
+    assert weights.shape == y_data.shape
+    assert_allclose(weights, expected_weights, rtol=1e-12, atol=1e-12)
+
+
+def expected_airpls(y, baseline, iteration):
+    """
+    The weighting for adaptive iteratively reweighted penalized least squares (airPLS).
+
+    Does not perform error checking since this is just used for simple weighting cases.
+
+    Parameters
+    ----------
+    y : numpy.ndarray, shape (N,)
+        The measured data.
+    baseline : numpy.ndarray, shape (N,)
+        The calculated baseline.
+    iteration : int
+        The iteration number. Should be 1-based, such that the first iteration is 1
+        instead of 0.
+
+    Returns
+    -------
+    weights : numpy.ndarray, shape (N,)
+        The calculated weights.
+    residual_l1_norm : float
+        The L1 norm of the negative residuals, used to calculate the exit criteria
+        for the airPLS algorithm.
+    exit_early : bool
+        Designates if there is a potential error with the calculation such that no further
+        iterations should be performed.
+
+    References
+    ----------
+    Zhang, Z.M., et al. Baseline correction using adaptive iteratively
+    reweighted penalized least squares. Analyst, 2010, 135(5), 1138-1146.
+
+    Notes
+    -----
+    Equation 9 in the original algorithm was misprinted according to the author
+    (https://github.com/zmzhang/airPLS/issues/8), so the correct weighting is used here.
+
+    The pybaselines weighting differs from the original airPLS algorithm by dividing the
+    weights by their maximum value to ensure that weights are within [0, 1]; the original
+    algorithm allowed weights to be greater than 1 which does not make sense mathematically
+    and does not match any other Whittaker-smoothing based algorithms.
+
+    """
+    residual = y - baseline
+    neg_mask = residual < 0
+    neg_residual = residual[neg_mask]
+    residual_l1_norm = abs(neg_residual).sum()
+
+    new_weights = np.exp((-iteration / residual_l1_norm) * neg_residual)
+    # Not stated in the paper, but without dividing by the maximum weight, the
+    # calculated weights can be greater than 1 which does not make sense mathematically
+    weights = np.zeros_like(y)
+    weights[neg_mask] = new_weights / new_weights.max()
+
+    return weights
+
+
+@pytest.mark.parametrize('iteration', (1, 10))
+@pytest.mark.parametrize('two_d', (True, False))
+def test_airpls_normal(iteration, two_d):
+    """Ensures airpls weighting works as intented for a normal baseline."""
+    if two_d:
+        y_data, baseline = baseline_1d_normal()
+    else:
+        y_data, baseline = baseline_2d_normal()
+
+    weights, residual_l1_norm, exit_early = _weighting._airpls(y_data, baseline, iteration)
+    expected_weights = expected_airpls(y_data, baseline, iteration)
+
+    residual = y_data - baseline
+    expected_residual_l1_norm = abs(residual[residual < 0].sum())
+
+    assert isinstance(weights, np.ndarray)
+    assert weights.shape == y_data.shape
+    assert_allclose(weights, expected_weights, rtol=1e-12, atol=1e-12)
+    assert_allclose(residual_l1_norm, expected_residual_l1_norm, rtol=1e-12, atol=1e-12)
+    assert not exit_early
+
+
+@pytest.mark.parametrize('iteration', (1, 10))
+@pytest.mark.parametrize('two_d', (True, False))
+def test_airpls_all_above(iteration, two_d):
+    """Ensures airpls weighting works as intented for a baseline with all points above the data."""
+    if two_d:
+        y_data, baseline = baseline_1d_all_above()
+    else:
+        y_data, baseline = baseline_2d_all_above()
+    weights, residual_l1_norm, exit_early = _weighting._airpls(y_data, baseline, iteration)
+    expected_weights = expected_airpls(y_data, baseline, iteration)
+    expected_residual_l1_norm = abs(y_data - baseline).sum()  # all residuals should be negative
+
+    assert isinstance(weights, np.ndarray)
+    assert weights.shape == y_data.shape
+    assert_allclose(weights, expected_weights, rtol=1e-12, atol=1e-12)
+    assert_allclose(residual_l1_norm, expected_residual_l1_norm, rtol=1e-12, atol=1e-12)
+    assert not exit_early
+
+
+@pytest.mark.parametrize('iteration', (1, 10))
+@pytest.mark.parametrize('two_d', (True, False))
+def test_airpls_all_below(iteration, two_d):
+    """Ensures airpls weighting works as intented for a baseline with all points below the data."""
+    if two_d:
+        y_data, baseline = baseline_1d_all_below()
+    else:
+        y_data, baseline = baseline_2d_all_below()
+
+    with pytest.warns(utils.ParameterWarning):
+        weights, residual_l1_norm, exit_early = _weighting._airpls(y_data, baseline, iteration)
+    expected_weights = np.zeros_like(y_data)
+
+    assert isinstance(weights, np.ndarray)
+    assert weights.shape == y_data.shape
+    assert_allclose(weights, expected_weights, rtol=1e-12, atol=1e-12)
+    assert_allclose(residual_l1_norm, 0.0)
+    assert exit_early
+
+
+@pytest.mark.parametrize('two_d', (True, False))
+@pytest.mark.parametrize('dtype', (float, np.float16, np.float32))
+def test_airpls_overflow(two_d, dtype):
+    """Ensures exponential overflow does not occur from airpls weighting."""
+    if two_d:
+        y_data, baseline = baseline_1d_normal()
+    else:
+        y_data, baseline = baseline_2d_normal()
+
+    y_data = y_data.astype(dtype)
+    baseline = baseline.astype(dtype)
+
+    residual = y_data - baseline
+    neg_mask = residual < 0
+    neg_residual = residual[neg_mask]
+    expected_residual_l1_norm = abs(neg_residual).sum()
+
+    # lowest iteration needed to cause exponential overflow for airpls weighting, plus 1
+    iteration = (
+        1
+        + np.log(np.finfo(y_data.dtype).max) * expected_residual_l1_norm / abs(neg_residual).max()
+    )
+
+    # sanity check to ensure overflow actually should occur and that it produces nan weights
+    with pytest.warns(RuntimeWarning):
+        expected_weights = expected_airpls(y_data, baseline, iteration)
+    assert (~np.isfinite(expected_weights)).any()
+
+    with np.errstate(over='raise'):
+        weights, residual_l1_norm, exit_early = _weighting._airpls(y_data, baseline, iteration)
+
+    assert np.isfinite(weights).all()
+    assert not exit_early
+    assert_allclose(residual_l1_norm, expected_residual_l1_norm, rtol=1e-12, atol=1e-12)
+