e7772850d928fc1604f144547351e2844dd30a21
Donnie Erb <55961724+derb12@users.noreply.github.com>
2026-06-07 12:32:27 -0400

    DOC: Simplify beads param selection example

--- docs/examples/misc/plot_beads_param_selection.py
+++ docs/examples/misc/plot_beads_param_selection.py
@@ -7,9 +7,9 @@ This example examines the parameter selection criteria for :meth:`~pybaselines.B
 proposed by `Navarro-Huerta, J.A., et al. "Assisted baseline subtraction in complex chromatograms
 using the BEADS algorithm" <https://doi.org/10.1016/j.chroma.2017.05.057>`_. Note that the
 preprocessing of data using a parabola before performing `beads` from the same paper is examined
-in  :ref:`another example <sphx_glr_generated_examples_misc_plot_beads_preprocessing.py>`.
+in :ref:`another example <sphx_glr_generated_examples_misc_plot_beads_preprocessing.py>`.
 
-The proposed parameter selection process works by varying the parameter of interest (eg.
+The proposed parameter selection process works by varying the parameter of interest (e.g.
 `freq_cutoff`, `lam_0`, `lam_1`, etc.) and calculating the autocorrelation coefficient,
 :math:`r^2`, from the residual, ``data - baseline``, for each fit. The autocorrelation plots
 will show plateau regions in which the calculated baseline is fairly consistent with regards
@@ -42,8 +42,6 @@ def autocorrelation(residual):
 x, y, baseline = make_data(return_baseline=True)
 baseline_fitter = Baseline(x)
 
-fixed_kwargs = {'tol': 1e-3, 'asymmetry': 6}
-
 # %%
 # Before varying any parameters, the input data will first be transformed by taking
 # the log of the input. The benefit of this transformation for `beads` is twofold:
@@ -53,11 +51,10 @@ fixed_kwargs = {'tol': 1e-3, 'asymmetry': 6}
 #   difference compared to the baseline.
 #
 # * The regularization parameters for `beads` penalize the L1 norms, so they are not
-#   scale-invariant compared to methods like Whittaker smoothing which penalize the L2
-#   norm; this means that fitting ``y`` and ``100 * y`` would require changing the various
-#   `lam_d` parameters. Taking the log transform helps to put all data on the same
+#   scale-invariant, which means that fitting ``y`` and ``100 * y`` would require changing
+#   the various `lam_d` parameters. Taking the log transform helps to put all data on the same
 #   relative scale such that the necessary regularization parameters between different
-#   datasets will not vary as much as long as other factors remain constant (eg. the number
+#   datasets will not vary as much as long as other factors remain constant (e.g. the number
 #   of points, baseline curvature, etc.).
 min_y = y.min()
 y_transf = np.log(y - min_y + 1)  # offset to ensure all values > 0 to prevent issues with log
@@ -65,30 +62,24 @@ y_transf = np.log(y - min_y + 1)  # offset to ensure all values > 0 to prevent i
 # %%
 # The first parameter to vary will be the cutoff frequency, `freq_cutoff`, since it typically
 # has the most influence on the baseline fit. A general rule of thumb is a higher cutoff
-# frequency is required for baselines with more curvature.
+# frequency is required for more flexible baselines; in other words, a sinusoidal baseline
+# would required a higher `freq_cutoff` than a linear baseline.
 #
 # The various regularization parameters `lam_d` will be initially fixed, with their values set
-# according to the L1 norm of each d'th derivative, as suggested by the `original BEADS manuscript
-# <https://doi.org/10.1016/j.chemolab.2014.09.014>`_. This can be done manually, like so::
-#
-#     alpha = 1.
-#     lam_0 = alpha / abs(y_transf).sum()  # np.diff(y_transf, 0) == y_transf
-#     lam_1 = alpha / abs(np.diff(y_transf, 1)).sum()
-#     lam_2 = alpha / abs(np.diff(y_transf, 2)).sum()
-#
-# If using pybaselines version 1.3 or later, this can be done more simply by setting the
-# `alpha` parameter and leaving all `lam_d` values to their default values of ``None``,
-# which is what this example will do.
-cutoff_freqs = np.geomspace(1e-3, 0.4, 30)
-alpha = 1.
+# according to the L1 norm of each d'th derivative, as suggested by the `original BEADS paper
+# <https://doi.org/10.1016/j.chemolab.2014.09.014>`_.
+lam_0 = 1 / abs(y_transf).sum()  # np.diff(y_transf, 0) == y_transf
+lam_1 = 1 / abs(np.diff(y_transf, 1)).sum()
+lam_2 = 1 / abs(np.diff(y_transf, 2)).sum()
 
 plt.figure()
 plt.plot(y)
 r_squares = []
 min_mse = np.inf
+cutoff_freqs = np.geomspace(1e-3, 0.4, 30)
 for i, cutoff_freq in enumerate(cutoff_freqs):
     fit_transf, params = baseline_fitter.beads(
-        y_transf, freq_cutoff=cutoff_freq, alpha=alpha, **fixed_kwargs
+        y_transf, freq_cutoff=cutoff_freq, lam_0=lam_0, lam_1=lam_1, lam_2=lam_2
     )
     fit = np.exp(fit_transf) + min_y - 1
     r_squares.append(autocorrelation(y - fit))
@@ -126,56 +117,13 @@ ax.set_xlabel('Frequency cutoff')
 
 print(f'Best cutoff frequency: {cutoff_freqs[best_index]:.2e}')
 
-# %%
-# Visual inspection of the autocorrelation plot shows that the best cutoff frequency, selected
-# as the middle of the last plateau, occurs at ~0.01, which matches with the cutoff frequency
-# that produced the lowest mean squared error with the known baseline. Now, the cutoff
-# frequency can be fixed, and the optimal `alpha` value can be found.
-cutoff_freq = cutoff_freqs[best_index]
-alphas = np.geomspace(5e-4, 50000, 30)
-
-plt.figure()
-plt.plot(y)
-r_squares = []
-min_mse = np.inf
-for i, alpha in enumerate(alphas):
-    fit_transf, params = baseline_fitter.beads(
-        y_transf, alpha=alpha, freq_cutoff=cutoff_freq, **fixed_kwargs
-    )
-    fit = np.exp(fit_transf) + min_y - 1
-    r_squares.append(autocorrelation(y - fit))
-    if i % 6 == 0:
-        plt.plot(fit, '--', label=f'alpha={alpha:.1e}')
-    mse = ((fit - baseline)**2).mean()
-    if mse < min_mse:
-        best_index = i
-        min_mse = mse
-        best_fit = fit
-
-plt.legend()
-
-plt.figure()
-plt.plot(y)
-plt.plot(best_fit, '--', label='best fit')
-plt.plot(baseline, ':', label='true baseline')
-plt.legend()
-
-plt.figure()
-plt.plot(alphas, r_squares, 'o-')
-plt.plot(alphas[best_index], r_squares[best_index], 'ro', label='best fit')
-plt.legend()
-plt.semilogx()
-plt.ylabel(r'Autocorrelation, $r^2$')
-plt.xlabel('alpha')
-
-print(f'Best alpha: {alphas[best_index]:.2e}')
-
 plt.show()
 # %%
-# Again, visual inspection of the autocorrelation plot shows an optimal `alpha` value of
-# approximately 1, matching the fit that produces the lowest mean squared error with the
-# known baseline. It is unsurprising that the optimal `alpha` value is approximately 1
-# since the optimal `freq_cutoff` value was originally selected using a fixed value of
-# ``alpha=1``. Note that instead of varying `alpha`, each individual `lam_d` value could
-# be varied instead. As noted by Navarro-Huerta, J.A., et al., typically `lam_0` has
-# the highest influence of the three regularization parameters.
+# Visual inspection of the autocorrelation plot above suggests that the best cutoff frequency,
+# selected as the middle of the last plateau, occurs at ~0.01, which matches with the cutoff
+# frequency that produced the lowest mean squared error with the known baseline, labeled as
+# "best fit" in the plots above.
+#
+# As noted at the start of this example, this same procedure of finding plateaus on the
+# autocorrelation plot can be repeated to select appropriate values for other parameters
+# for `beads`, including `lam_0`, `lam_1`, `lam_2`, and `asymmetry`.
