MAINT: Fix improper masking for iasls D1.T @ D1 @ y calc
Previous way was essentially doing M1 @ (D.T @ D @ y) rather than (D.T @ M2 @ D @ y) , where M1 and M2 just define where zeros should go to ignore regions affected by the mask. Latter expression makes more sense when building the linear system. The residual penalty matrix is also changed to reflect the masking rather than ignoring.

pybaselines/_banded_utils.py
@@ -405,7 +405,7 @@ def _banded_dot_vector(ab, x, lower=True):
     return output
 
 
-def difference_matrix(data_size, diff_order=2, diff_format=None):
+def difference_matrix(data_size, diff_order=2, diff_format=None, making_penalty=False):
     """
     Creates an n-th order finite-difference matrix.
 
@@ -418,6 +418,11 @@ def difference_matrix(data_size, diff_order=2, diff_format=None):
     diff_format : str or None, optional
         The sparse format to use for the difference matrix. Default is None,
         which will use the default specified in :func:`scipy.sparse.diags`.
+    making_penalty : bool, optional
+        If False (default), will ignore the case where ``diff_order > data_size``
+        creates an empty matrix, to match the behavior of ``numpy.diff``. If True, will
+        raise since it results in a penalty matrix of 0, which is equivalent to using
+        a regularization parameter of 0 and is not allowed.
 
     Returns
     -------
@@ -427,7 +432,8 @@ def difference_matrix(data_size, diff_order=2, diff_format=None):
     Raises
     ------
     ValueError
-        Raised if `diff_order` or `data_size` is negative.
+        Raised if `diff_order` or `data_size` is negative. Also raised if
+        ``diff_order > data_size`` and `making_penalty` is True.
 
     Notes
     -----
@@ -462,8 +468,13 @@ def difference_matrix(data_size, diff_order=2, diff_format=None):
     elif data_size < 0:
         raise ValueError('data size must be >= 0')
     elif diff_order > data_size:
-        # do not issue warning or exception to maintain parity with np.diff
-        diff_order = data_size
+        if making_penalty:
+            # if using for making penalty matrix, diff order > data size results
+            # in a penalty of zero, which is equivalent to lam=0 and not allowed
+            raise ValueError('data size must be greater than the difference order')
+        else:
+            # do not issue warning or exception to maintain parity with np.diff
+            diff_order = data_size
 
     if diff_order == 0:
         # faster to directly create identity matrix
@@ -699,7 +710,7 @@ def diff_penalty_diagonals(data_size, diff_order=2, lower_only=True, padding=0):
     if diff_order == 0:
         diagonals = np.ones((1, data_size))
     elif data_size < 2 * diff_order + 1 or diff_order > 3:
-        diff_matrix = difference_matrix(data_size, diff_order, 'csc')
+        diff_matrix = difference_matrix(data_size, diff_order, 'csc', making_penalty=True)
         diagonals = _sparse_to_banded(diff_matrix.T @ diff_matrix)[0]
         if lower_only:
             diagonals = diagonals[diff_order:]
@@ -712,12 +723,17 @@ def diff_penalty_diagonals(data_size, diff_order=2, lower_only=True, padding=0):
     return diagonals
 
 
-def diff_penalty_matrix(data_size, diff_order=2, diff_format='csr'):
+def diff_penalty_matrix(data_size, diff_order=2, diff_format='csr', mask=None):
     """
     Creates the finite difference penalty matrix.
 
     If `D` is the finite difference matrix, then the finite difference penalty
     matrix is defined as ``D.T @ D``.
+
+    If a finite difference is applied to a vector y with a corresponding mask, such as for iasls,
+    then that penalty changes from ``||D @ y||^2`` to ``||M @ D @ y||^2``, where M ignores all
+    affected rows in ``D @ y``. The resulting term in the linear system changes to
+    ``D.T @ M.T @ M @ D @ y`` or simply ``D.T @ M @ D @ y`` since M is diagonal and binary.
 
     Parameters
     ----------
@@ -726,17 +742,15 @@ def diff_penalty_matrix(data_size, diff_order=2, diff_format='csr'):
     diff_order : int, optional
         The integer differential order; must be >= 0. Default is 2.
     diff_format : str or None, optional
-        The sparse format to use for the difference matrix. Default is 'csr'.
+        The sparse format to use for the penalty matrix. Default is 'csr'.
+    mask : array-like, shape (`data_size`,), optional
+        A Boolean array, in which all indices that are ``True`` denote indices that should
+        be ignored for `y`. If None (default), denotes all values should be included.
 
     Returns
     -------
     penalty_matrix : scipy.sparse.spmatrix or scipy.sparse.sparray
         The sparse difference penalty matrix.
-
-    Raises
-    ------
-    ValueError
-        Raised if `diff_order` is not greater than `data_size`.
 
     Notes
     -----
@@ -750,14 +764,26 @@ def diff_penalty_matrix(data_size, diff_order=2, diff_format='csr'):
     without the matrix multiplication.
 
     """
-    if data_size <= diff_order:
-        raise ValueError('data size must be greater than the difference order.')
-    penalty_bands = diff_penalty_diagonals(data_size, diff_order, lower_only=False)
-    penalty_matrix = dia_object(
-        (penalty_bands, np.arange(diff_order, -diff_order - 1, -1)), shape=(data_size, data_size),
-    ).asformat(diff_format)
+    if mask is None:
+        penalty_bands = diff_penalty_diagonals(data_size, diff_order, lower_only=False)
+        penalty_matrix = dia_object(
+            (penalty_bands, np.arange(diff_order, -diff_order - 1, -1)),
+            shape=(data_size, data_size)
+        )
+    else:
+        fit_mask = np.logical_not(mask)
+        for _ in range(diff_order):
+            fit_mask = fit_mask[1:] & fit_mask[:-1]
 
-    return penalty_matrix
+        # TODO might be nice to generalize this part once array-like lam values are allowed,
+        # since it uses the same calc
+
+        # same as D.T @ diags(fit_mask) @ D but faster; works for both sparse matrices and
+        # sparse arrays
+        diff_matrix = difference_matrix(data_size, diff_order, 'csc', making_penalty=True)
+        penalty_matrix = diff_matrix.T @ diff_matrix.multiply(fit_mask[:, None])
+
+    return penalty_matrix.asformat(diff_format, copy=False)
 
 
 class PenalizedSystem:

pybaselines/spline.py
@@ -13,14 +13,16 @@ from scipy.ndimage import grey_opening
 
 from . import _weighting
 from ._algorithm_setup import _Algorithm, _class_wrapper
-from ._banded_utils import _add_diagonals, _shift_rows, _sparse_to_banded, diff_penalty_matrix
+from ._banded_utils import (
+    _add_diagonals, _shift_rows, _sparse_to_banded, diff_penalty_matrix
+)
 from ._nd.pls import _PLSNDMixin
 from ._spline_utils import _basis_midpoints
 from ._validation import (
     _check_lam, _check_optional_array, _check_scalar_variable, _check_spline_degree
 )
 from .results import PSplineResult
-from .utils import _masked_matvec, _sort_array, relative_difference
+from .utils import _sort_array, relative_difference
 
 
 class _Spline(_Algorithm, _PLSNDMixin):
@@ -364,12 +366,9 @@ class _Spline(_Algorithm, _PLSNDMixin):
         # B.T @ D_1.T @ D_1 @ B and B.T @ D_1.T @ D_1 @ y
         d1_penalty = (
             pspline.basis.basis.T
-            @ (_check_lam(lam_1) * diff_penalty_matrix(self._size, 1))
+            @ (_check_lam(lam_1) * diff_penalty_matrix(self._size, 1, mask=self.mask))
         )
-        if self.mask is None:
-            partial_rhs = d1_penalty @ y
-        else:
-            partial_rhs = _masked_matvec(d1_penalty, y, self.mask)
+        partial_rhs = d1_penalty @ y
         # now change d1_penalty back to banded array
         d1_penalty = _sparse_to_banded(d1_penalty @ pspline.basis.basis)[0]
         if pspline.lower:

pybaselines/two_d/_whittaker_utils.py
@@ -15,7 +15,7 @@ from scipy.linalg import (
 from scipy.sparse import issparse, kron
 from scipy.sparse.linalg import factorized, spsolve
 
-from .._banded_utils import diff_penalty_diagonals, diff_penalty_matrix
+from .._banded_utils import diff_penalty_diagonals, diff_penalty_matrix, difference_matrix
 from .._compat import identity
 from .._validation import _check_lam, _check_scalar, _check_scalar_variable
 from ..utils import ParameterWarning
@@ -52,6 +52,65 @@ def _face_splitting(basis):
     else:
         output = np.kron(basis, ones) * (np.kron(ones, basis))
     return output
+
+
+def diff_penalty_matrix_2d(data_size, lam=1., diff_order=2, diff_format='csr', mask=None):
+    """
+    Creates the 2D finite difference penalty matrix.
+
+    Parameters
+    ----------
+    data_size : tuple[int, int]
+        The number of data points for the system along the rows and columns, respectively.
+    lam : float or tuple[float, float], optional
+        The penalty factor applied to the difference matrix for the rows and columns,
+        respectively. If a single value is given, both will use the same value. Larger
+        values produce smoother results. Must be greater than 0. Default is 1.
+    diff_order : int or tuple[int, int], optional
+        The difference order of the penalty for the rows and columns, respectively. If
+        a single value is given, both will use the same value.
+        Default is 2 (second order difference).
+    diff_format : str or None, optional
+        The sparse format to use for the penalty matrix. Default is 'csr'.
+    mask : array-like, shape (M, N), optional
+        A Boolean array, in which all indices that are `True` denote indices that should
+        be ignored. If None (default), denotes all values should be included.
+
+    Returns
+    -------
+    scipy.sparse.spmatrix or scipy.sparse.sparray
+        The penalty matrix. The final shape is
+        (``numpy.prod(data_size)``, ``numpy.prod(data_size)``).
+
+    """
+    diff_order = _check_scalar_variable(
+        diff_order, allow_zero=False, variable_name='difference order', two_d=True, dtype=int
+    )
+    rows, cols = data_size
+    lams = _check_lam(lam, two_d=True)
+    if mask is None:
+        penalty_rows = kron(diff_penalty_matrix(rows, diff_order[0]), identity(cols))
+        penalty_cols = kron(identity(rows), diff_penalty_matrix(cols, diff_order[1]))
+    else:
+        fit_mask_r = np.logical_not(mask)
+        fit_mask_c = fit_mask_r.copy()
+        for _ in range(diff_order[0]):
+            fit_mask_r = fit_mask_r[1:, :] & fit_mask_r[:-1, :]
+        for _ in range(diff_order[1]):
+            fit_mask_c = fit_mask_c[:, 1:] & fit_mask_c[:, :-1]
+
+        # TODO might be nice to generalize this part once array-like lam values are allowed,
+        # since it uses the same calc
+        D_rows = kron(
+            difference_matrix(rows, diff_order[0], 'csc', making_penalty=True), identity(cols)
+        )
+        D_cols = kron(
+            identity(rows), difference_matrix(cols, diff_order[1], 'csc', making_penalty=True)
+        )
+        penalty_rows = D_rows.T @ D_rows.multiply(fit_mask_r.ravel()[:, None])
+        penalty_cols = D_cols.T @ D_cols.multiply(fit_mask_c.ravel()[:, None])
+
+    return (lams[0] * penalty_rows + lams[1] * penalty_cols).asformat(diff_format, copy=False)
 
 
 class PenalizedSystem2D:
@@ -190,13 +249,9 @@ class PenalizedSystem2D:
         self.lam = _check_lam(lam, two_d=True)
         self.symmetric = symmetric
 
-        penalty_rows = diff_penalty_matrix(self._num_bases[0], self.diff_order[0])
-        penalty_columns = diff_penalty_matrix(self._num_bases[1], self.diff_order[1])
-
-        # multiplying lam by the Kronecker product is the same as multiplying just D.T @ D with lam
-        P_rows = kron(self.lam[0] * penalty_rows, identity(self._num_bases[1]))
-        P_columns = kron(identity(self._num_bases[0]), self.lam[1] * penalty_columns)
-        self.penalty = P_rows + P_columns
+        self.penalty = diff_penalty_matrix_2d(
+            self._num_bases, self.lam, self.diff_order, diff_format='csc'
+        )
 
         self._update_bands()
 

pybaselines/two_d/whittaker.py
@@ -13,9 +13,9 @@ from .._compat import diags
 from .._nd.pls import _PLSNDMixin
 from .._validation import _check_optional_array, _check_scalar_variable
 from ..results import WhittakerResult2D
-from ..utils import _masked_matvec, _sort_array2d, relative_difference
+from ..utils import _sort_array2d, relative_difference
 from ._algorithm_setup import _Algorithm2D
-from ._whittaker_utils import PenalizedSystem2D
+from ._whittaker_utils import PenalizedSystem2D, diff_penalty_matrix_2d
 
 
 class _Whittaker(_Algorithm2D, _PLSNDMixin):
@@ -193,14 +193,13 @@ class _Whittaker(_Algorithm2D, _PLSNDMixin):
             weights = _weighting._iasls(data - baseline.reshape(self._shape), p=p, mask=self.mask)
 
         y, weight_array, whittaker_system = self._setup_whittaker(data, lam, diff_order, weights)
-        penalized_system_1 = PenalizedSystem2D(self._shape, lam_1, diff_order=1)
+        residual_penalty = diff_penalty_matrix_2d(
+            self._shape, lam_1, diff_order=1, mask=self.mask
+        )
 
-        # (W.T @ W + P_1) @ y -> P_1 @ y + W.T @ W @ y
-        whittaker_system.add_penalty(penalized_system_1.penalty)
-        if self.mask is None:
-            p1_y = penalized_system_1.penalty @ y
-        else:
-            p1_y = _masked_matvec(penalized_system_1.penalty, y, self.mask.ravel())
+        # (W + P_1) @ y -> P_1 @ y + W @ y
+        whittaker_system.add_penalty(residual_penalty)
+        p1_y = residual_penalty @ y
 
         tol_history = np.empty(max_iter + 1)
         success = False
@@ -217,7 +216,7 @@ class _Whittaker(_Algorithm2D, _PLSNDMixin):
         params = {
             'weights': weight_array, 'tol_history': tol_history[:i + 1],
             'result': WhittakerResult2D(
-                whittaker_system, weight_array, rhs_extra=penalized_system_1.penalty
+                whittaker_system, weight_array, rhs_extra=residual_penalty
             ), 'success': success
         }
 

pybaselines/whittaker.py
@@ -10,11 +10,13 @@ import numpy as np
 
 from . import _weighting
 from ._algorithm_setup import _Algorithm, _class_wrapper
-from ._banded_utils import _shift_rows, diff_penalty_diagonals, diff_penalty_matrix
+from ._banded_utils import (
+    _shift_rows, _sparse_to_banded, diff_penalty_diagonals, diff_penalty_matrix
+)
 from ._nd.pls import _PLSNDMixin
 from ._validation import _check_lam, _check_optional_array, _check_scalar_variable
 from .results import WhittakerResult
-from .utils import relative_difference, _masked_matvec, _sort_array
+from .utils import relative_difference, _sort_array
 
 
 class _Whittaker(_Algorithm, _PLSNDMixin):
@@ -317,20 +319,25 @@ class _Whittaker(_Algorithm, _PLSNDMixin):
 
         y, weight_array, whittaker_system = self._setup_whittaker(data, lam, diff_order, weights)
         lambda_1 = _check_lam(lam_1)
-        residual_penalty = lambda_1 * diff_penalty_diagonals(
-            self._size, 1, whittaker_system.lower, padding=diff_order - 1
-        )
-        whittaker_system.add_penalty(residual_penalty)
         if self.mask is None:
             # fast calculation of (D_1.T @ D_1) @ y
             d1_y = y.copy()
             d1_y[0] = y[0] - y[1]
             d1_y[-1] = y[-1] - y[-2]
             d1_y[1:-1] = 2 * y[1:-1] - y[:-2] - y[2:]
+            residual_penalty = diff_penalty_diagonals(
+                self._size, 1, whittaker_system.lower, padding=diff_order - 1
+            )
         else:
-            d1_y = _masked_matvec(diff_penalty_matrix(self._size, 1, 'csr'), y, self.mask)
+            residual_penalty = diff_penalty_matrix(self._size, 1, mask=self.mask)
+            d1_y = residual_penalty @ y
+            residual_penalty = _sparse_to_banded(residual_penalty)[0]
+            if whittaker_system.lower:
+                residual_penalty = residual_penalty[residual_penalty.shape[0] // 2:]
 
-        d1_y = lambda_1 * d1_y
+        d1_y *= lambda_1
+        residual_penalty *= lambda_1
+        whittaker_system.add_penalty(residual_penalty)
         tol_history = np.empty(max_iter + 1)
         success = False
         for i in range(max_iter + 1):

tests/test_banded_utils.py
@@ -13,6 +13,7 @@ import numpy as np
 from numpy.testing import assert_allclose, assert_array_equal
 import pytest
 from scipy.linalg import cholesky_banded
+from scipy.sparse import issparse
 from scipy.sparse.linalg import spsolve
 
 from pybaselines import _banded_utils, _spline_utils, results
@@ -119,7 +120,7 @@ def test_diff_penalty_matrix(data_size, diff_order):
 
     output = _banded_utils.diff_penalty_matrix(data_size, diff_order)
 
-    assert_allclose(expected_matrix.toarray(), output.toarray(), rtol=1e-12, atol=1e-12)
+    assert_allclose(expected_matrix.toarray(), output.toarray(), rtol=1e-16, atol=1e-16)
 
 
 @pytest.mark.parametrize('data_size', (3, 6))
@@ -132,12 +133,56 @@ def test_diff_penalty_matrix_too_few_data(data_size, diff_order):
     if data_size <= diff_order:
         with pytest.raises(ValueError):
             _banded_utils.diff_penalty_matrix(data_size, diff_order)
-        # the actual matrix should be just zeros
-        actual_result = np.zeros((data_size, data_size))
-        assert_allclose(actual_result, expected_matrix.toarray(), rtol=1e-12, atol=1e-12)
     else:
         output = _banded_utils.diff_penalty_matrix(data_size, diff_order)
-        assert_allclose(output.toarray(), expected_matrix.toarray(), rtol=1e-12, atol=1e-12)
+        assert_allclose(output.toarray(), expected_matrix.toarray(), rtol=1e-16, atol=1e-16)
+
+
+@pytest.mark.parametrize('data_size', (10, 51))
+@pytest.mark.parametrize('diff_order', (1, 2, 3, 4))
+def test_diff_penalty_matrix_mask(data_size, diff_order):
+    """Ensures mask is correctly propagated for diff_penalty_matrix."""
+    rng = np.random.default_rng(1234)
+    y = rng.normal(0., 0.5, data_size)
+    mask = rng.choice([True, False], data_size, p=[0.3, 0.7])
+
+    # any point in the masked regions should propagate nan and be removed
+    diff_y = np.diff(np.where(mask, np.nan, y), diff_order)
+    nan_mask = np.isnan(diff_y)
+    diff_y[nan_mask] = 0
+
+    diff_matrix = _banded_utils.difference_matrix(data_size, diff_order)
+    expected_matvec = diff_matrix.T @ diff_y
+    expected_mat = diff_matrix.T @ diags(~nan_mask) @ diff_matrix
+
+    output = _banded_utils.diff_penalty_matrix(data_size, diff_order, mask=mask)
+    matvec = output @ y
+
+    assert issparse(output)
+    assert_allclose(output.toarray(), expected_mat.toarray(), rtol=0, atol=1e-16)
+    assert_allclose(matvec, expected_matvec, rtol=5e-14, atol=1e-16)
+
+
+@pytest.mark.parametrize('data_size', (10, 51))
+@pytest.mark.parametrize('diff_order', (1, 2, 3, 4))
+def test_diff_penalty_matrix_mask_fit_all(data_size, diff_order):
+    """Ensures a mask that fits all points should exactly reproduce D.T @ D."""
+    rng = np.random.default_rng(1234)
+    y = rng.normal(0., 0.5, data_size)
+    mask = np.zeros_like(y, dtype=bool)
+
+    diff_matrix = _banded_utils.difference_matrix(data_size, diff_order)
+    expected_mat = diff_matrix.T @ diff_matrix
+    expected_matvec = expected_mat @ y
+
+    output = _banded_utils.diff_penalty_matrix(data_size, diff_order, mask=mask)
+    matvec = output @ y
+
+    assert issparse(output)
+    assert_allclose(output.toarray(), expected_mat.toarray(), rtol=0, atol=1e-16)
+    # NOTE no clue why the two matvecs have such a large rtol difference when the two
+    # matrices are the exact same...
+    assert_allclose(matvec, expected_matvec, rtol=5e-14, atol=1e-16)
 
 
 def test_shift_rows_2_diags():
@@ -432,7 +477,8 @@ def test_difference_matrix_order_neg():
         _banded_utils.difference_matrix(10, diff_order=-2)
 
 
-def test_difference_matrix_order_over():
+@pytest.mark.parametrize('making_penalty', (True, False))
+def test_difference_matrix_order_over(making_penalty):
     """
     Tests the (n + 1)th order differential matrix against the actual representation.
 
@@ -441,10 +487,14 @@ def test_difference_matrix_order_over():
     following a similar logic as np.diff.
 
     """
-    diff_matrix = _banded_utils.difference_matrix(10, 11).toarray()
-    actual_matrix = np.empty(shape=(0, 10))
+    if making_penalty:
+        with pytest.raises(ValueError, match='data size must be greater'):
+            _banded_utils.difference_matrix(10, 11, making_penalty=making_penalty)
+    else:
+        diff_matrix = _banded_utils.difference_matrix(10, 11, making_penalty=making_penalty)
+        actual_matrix = np.empty(shape=(0, 10))
 
-    assert_array_equal(diff_matrix, actual_matrix)
+        assert_array_equal(diff_matrix.toarray(), actual_matrix)
 
 
 def test_difference_matrix_size_neg():

tests/two_d/test_whittaker_utils.py
@@ -16,7 +16,7 @@ from scipy.sparse import issparse, kron
 from scipy.sparse.linalg import spsolve
 
 from pybaselines._banded_utils import diff_penalty_diagonals
-from pybaselines._compat import csr_object, dia_object, identity
+from pybaselines._compat import csr_object, dia_object, diags, identity
 from pybaselines.two_d import _spline_utils, _whittaker_utils
 from pybaselines.utils import difference_matrix
 from pybaselines.results import WhittakerResult2D
@@ -176,6 +176,66 @@ def test_penalized_system_negative_lam_fails(small_data2d, lam):
     """Ensures a lam value less than or equal to 0 fails."""
     with pytest.raises(ValueError):
         _whittaker_utils.PenalizedSystem2D(small_data2d.shape, lam=lam)
+
+
+@pytest.mark.parametrize('data_size', ((10, 10), (12, 51), (31, 14)))
+@pytest.mark.parametrize('diff_order', (1, 2, 3, 4, (2, 3)))
+def test_diff_penalty_matrix_2d_mask(data_size, diff_order):
+    """Ensures mask is correctly propagated for diff_penalty_matrix_2d."""
+    rows, cols = data_size
+    rng = np.random.default_rng(1234)
+    y = rng.normal(0., 0.5, data_size)
+    mask = rng.choice([True, False], data_size, p=[0.3, 0.7])
+    if isinstance(diff_order, int):
+        diff_r = diff_order
+        diff_c = diff_order
+    else:
+        diff_r, diff_c = diff_order
+
+    # any point in the masked regions should propagate nan and be removed
+    y_nan = np.where(mask, np.nan, y)
+    diff_y_r = np.diff(y_nan, diff_r, axis=0)
+    diff_y_c = np.diff(y_nan, diff_c, axis=1)
+    nan_mask_r = np.isnan(diff_y_r)
+    diff_y_r[nan_mask_r] = 0
+    nan_mask_c = np.isnan(diff_y_c)
+    diff_y_c[nan_mask_c] = 0
+
+    D_rows = kron(difference_matrix(rows, diff_r, 'csc'), identity(cols))
+    D_cols = kron(identity(rows), difference_matrix(cols, diff_c, 'csc'))
+    expected_mat = (
+        D_rows.T @ diags(~nan_mask_r.ravel()) @ D_rows
+        + D_cols.T @ diags(~nan_mask_c.ravel()) @ D_cols
+    )
+    expected_matvec = D_rows.T @ diff_y_r.ravel() + D_cols.T @ diff_y_c.ravel()
+
+    output = _whittaker_utils.diff_penalty_matrix_2d(data_size, diff_order=diff_order, mask=mask)
+    matvec = output @ y.ravel()
+
+    assert issparse(output)
+    assert_allclose(output.toarray(), expected_mat.toarray(), rtol=0, atol=1e-16)
+    assert_allclose(matvec, expected_matvec, rtol=5e-14, atol=1e-16)
+
+
+@pytest.mark.parametrize('data_size', ((10, 10), (12, 51), (31, 14)))
+@pytest.mark.parametrize('diff_order', (1, 2, 3, 4, (2, 3)))
+def test_diff_penalty_matrix_2d_mask_fit_all(data_size, diff_order):
+    """Ensures a mask that fits all points should exactly reproduce the penalty matrix."""
+    rng = np.random.default_rng(1234)
+    y = rng.normal(0., 0.5, data_size)
+    mask = np.zeros_like(y, dtype=bool)
+
+    expected_mat = _whittaker_utils.diff_penalty_matrix_2d(data_size, diff_order=diff_order)
+    expected_matvec = expected_mat @ y.ravel()
+
+    output = _whittaker_utils.diff_penalty_matrix_2d(data_size, diff_order=diff_order, mask=mask)
+    matvec = output @ y.ravel()
+
+    assert issparse(output)
+    assert_allclose(output.toarray(), expected_mat.toarray(), rtol=0, atol=1e-16)
+    # NOTE no clue why the two matvecs have such a large rtol difference when the two
+    # matrices are the exact same...
+    assert_allclose(matvec, expected_matvec, rtol=5e-13, atol=1e-16)
 
 
 @pytest.mark.parametrize('diff_order', (1, 2, 3, [1, 3]))